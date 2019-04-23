a2a4cdbd2ae10237f72a2fd648ffe5d8e1055ec4
Augustine Kim <ajk830@gmail.com>
2019-04-23 12:47:43 -0700

    added userController, database

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,19 +1,82 @@
-const { Pool, Client } = require('pg');
-const connectionString = "";
+const bcrypt = require ('bcryptjs');
+const SALT_WORK_FACTOR = 10;
 
-const pool = new Pool({
-  connectionString: connectionString
-});
+const pool = require ('./db');
 
-const client = new Client({
-  connectionString: connectionString
-})
-client.connect()
-  .then( () => console.log('Connected to database'))
+userController = {};
 
-client.query('SELECT * from users;', (err, res) => {
-  if (err) console.log('err:', err)
-  if (res) console.log('res.rows:', res.rows)
-  client.end()
-})
+userController.createUser = (req, res, next) => {
+  const { username, password } = req.body;
+  password = bcrypt.hashSync(password, SALT_WORK_FACTOR);
+  pool.query(`
+    INSERT INTO Users (username, password)
+    VALUES ($1, $2)
+    RETURNING *
+  `, [ username, password ])
+  .then(resp => {
+    console.log('row after creating user', resp.rows[0]);
+    res.locals.user = resp.rows[0];
+    next();
+  })
+  .catch(err => {
+    console.error(err.stack);
+    next('DB create user error');
+  })
+}
 
+userController.verifyUser = (req, res, next) => {
+  const { username, password } = req.body;
+  pool.query(`
+      SELECT * FROM Users
+      WHERE username=$1
+    `, [username])
+    .then(resp => {
+      if (resp.rows[0] && bcrypt.compareSync(password, resp.rows[0].password)) next();
+      else res.status(401).send('Incorrect username/password');
+    })
+    .catch(err => {
+      console.error(err.stack);
+      next('DB verify user error');
+    })
+}
+
+userController.logoutUser = (req, res, next) => {
+console.log('logging out');
+}
+
+userController.submitSnippet = (req, res, next) => {
+  const { name, content, created_by } = req.body;
+  pool.query(`INSERT INTO Snippets VALUES ($1, $2, $3) RETURNING *`, [name, content, created_by])
+    .then((resp) => {
+      console.log('row after submitting snippet', resp.rows[0])
+      next();
+    })
+    .catch(err => {
+      console.error(err)
+      return next('Error submitting snippet');
+    })
+}
+
+userController.favoriteSnippet = (req, res, next) => {
+  const { snippetId, userId } = req.body;
+  pool.query(`UPDATE Snippets SET num_faves = num_faves + 1 WHERE snippet_id = ${snippetId}`)
+    .then((resp) => {
+      console.log('row after favoriting snippet', resp.rows[0])
+    })
+    .catch(err => {
+      console.error(err)
+      return next('Error favoriting snippet');
+    })
+
+  pool.query(`INSERT INTO UserFaveSnippets ($1, $2)`, [snippetId, userId])
+    .then((resp) => {
+      console.log('row after favoriting snippet in join table', resp.rows[0])
+      next();
+    })
+    .catch(err => {
+      console.log(err);
+      return next('Error favoriting snippet');
+    })
+}
+
+module.exports = userController;
--- server/server.js
+++ server/server.js
@@ -1,15 +1,47 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const bodyparser = require('body-parser');
+const cookieparser = require('cookie-parser');
+
+const sessionController = require('./controllers/sessionController');
+const userController = require('./controllers/userController');
 
 const PORT = 3000;
 
-app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname, '../client/index.html'));
+app.use(bodyparser.json(), cookieparser());
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/index.html'));
 });
 
-app.use('*', (req,res) => {
+app.post('/signup', userController.createUser, (req, res) => {
+  res.send('Success');
+});
+
+app.post('/submitsnippet', userController.submitSnippet, (req, res) => {
+  res.send('Successfully submitted snippet')
+})
+
+app.put('/favesnippet', userController.favoriteSnippet, (req, res) => {
+  res.send('Liked Snippet')
+})
+
+app.post('/login', (req, res) => {
+  res.send('Success');
+});
+
+app.use('*', (req, res) => {
     res.status(404).send('Page Not Found');
 });
 
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong');
+})
+
 app.listen(PORT, () => {console.log(`Listening on port ${PORT}...`)});
+
+//   (\__/)
+//  (='.'=)
+// ('')_('')
